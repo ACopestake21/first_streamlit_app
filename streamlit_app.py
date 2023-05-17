ac2023367e12a278dbede524fb35200290dec2d6
ACopestake21 <123544682+ACopestake21@users.noreply.github.com>
2023-05-17 17:14:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,20 +26,21 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # Get user's fruit choice
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+
+# streamlit.write('The user entered ', fruit_choice)
 
 # API Request
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+    if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+    else:
+      fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
+      fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
+      
 streamlit.text(fruityvice_response)
-
-# Normalize the json file
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# Display normalized data
-streamlit.dataframe(fruityvice_normalized)
-
-
 streamlit.stop()
 
 
